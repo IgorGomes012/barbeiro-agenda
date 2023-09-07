c7dfc75edf163ba2b197335f054c19d5ae3e5c20
igor gomes <gomesoliveirasl02@hotmail.com>
2023-09-07 09:18:08 -0300

    fix: style page

---
+++ src/app/components/header/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderPage = styled.div`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid white;
+`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Headerr from './components/header'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>oi</h1>
     </main>
   )
 }
