23862dc7489841beac061f6806402876f1653ff4
igor gomes <gomesoliveirasl02@hotmail.com>
2023-09-08 18:47:26 -0300

    add logo in banner

--- src/app/components/Banner/style.js
+++ src/app/components/Banner/style.js
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 export const ContainerBanner = styled.div` 
   background-color: blue;
   width: 100%;
-  height: 500px;
+  height: 400px;
 `;
 
 export const ContainerImage = styled.div` 
 width: 100%;
 height: 100%;
 position: relative;
+`;
+export const LogoBanner = styled.div` 
+display: flex;
+justify-content: center;
+margin: -120px 0 0 0
 `;
