1867ca09e1d3263b241ebced387f17921148b3b3
igor gomes <gomesoliveirasl02@hotmail.com>
2023-09-11 20:18:01 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 
-import Banner from './components/Banner'
-import Headerr from './components/header'
+import Banner from "./components/Banner";
+import Headerr from "./components/header";
 
 export default function Home() {
   return (
     <>
       <Headerr />
       <Banner />
+      <h1>teste</h1>
     </>
-  )
+  );
 }
